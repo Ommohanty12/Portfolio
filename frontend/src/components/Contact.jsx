d863b8da6bd2c42f207ad094008a3a97580695be
Ommohanty12 <ommohanty1210@gmail.com>
2026-04-16 07:37:32 +0530

    commi

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
         setStatus('loading');
 
         try {
-            const res = await fetch('https://portfolio-d0m7.onrender.com/contact', {
+            const res = await fetch('https://portfolio-1-ekza.onrender.com/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
